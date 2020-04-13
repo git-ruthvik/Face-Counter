ec5da0e768357ffd599b832a77ddfd3f0d3d7852
Ruthvik Rao <49203819+git-ruthvik@users.noreply.github.com>
2020-04-13 13:53:50 +0530

    Update facecounter.py

--- facecounter.py
+++ facecounter.py
@@ -4,7 +4,7 @@ import datetime
 file=open('counter1234.txt','w')
 #otpt=""
 # Load the cascade
-face_cascade = cv2.CascadeClassifier('./haarcascade_frontalface_default.xml')
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
 # To capture video from webcam. 
 cap = cv2.VideoCapture(1)
@@ -14,15 +14,20 @@ while True:
     _, img = cap.read()
     # Convert to grayscale
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    
     # Detect the faces
     faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+    
     # Draw the rectangle around each face
     for (x, y, w, h) in faces:
         cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+    
     # Display
     cv2.imshow('img', img)
     count=str(len(faces))
     otpt = str('Time={}, Count={}'.format(datetime.datetime.now(),count))
+    
+    # Wirte into txt file
     file.write(otpt)
     file.write('\n')
 
