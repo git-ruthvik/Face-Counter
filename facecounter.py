e5ed878e4587452dd2d9daeeb91eaf41d48ec45b
Ruthvik Rao <49203819+git-ruthvik@users.noreply.github.com>
2020-04-13 13:51:59 +0530

    Add files via upload

---
+++ facecounter.py
@@ -0,0 +1,34 @@
+import cv2
+import datetime
+
+file=open('counter1234.txt','w')
+#otpt=""
+# Load the cascade
+face_cascade = cv2.CascadeClassifier('./haarcascade_frontalface_default.xml')
+
+# To capture video from webcam. 
+cap = cv2.VideoCapture(1)
+
+while True:
+    # Read the frame
+    _, img = cap.read()
+    # Convert to grayscale
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # Detect the faces
+    faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+    # Draw the rectangle around each face
+    for (x, y, w, h) in faces:
+        cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+    # Display
+    cv2.imshow('img', img)
+    count=str(len(faces))
+    otpt = str('Time={}, Count={}'.format(datetime.datetime.now(),count))
+    file.write(otpt)
+    file.write('\n')
+
+    # Stop if escape key is pressed
+    k = cv2.waitKey(30) & 0xff
+    if k==27:
+        break
+# Release the VideoCapture object
+cap.release()
